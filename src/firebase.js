cbc19ba78e0738219daa0445c1004de9045d7d72
nihaltowfiq <nihaltowfiq@gmail.com>
2020-11-29 03:54:11 +0600

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCeY4taC8IBU2tJJF3Rf-GRbWws8t4nlcQ",
+    authDomain: "whatsapp-clone-teamwork.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-teamwork.firebaseio.com",
+    projectId: "whatsapp-clone-teamwork",
+    storageBucket: "whatsapp-clone-teamwork.appspot.com",
+    messagingSenderId: "368504895413",
+    appId: "1:368504895413:web:14c1697d34649aae910bfc"
+};
